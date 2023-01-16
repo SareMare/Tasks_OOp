00cc30c04dfb5c94a6fb9293743ece0eec37ab7a
Andrew <115143581+SareMare@users.noreply.github.com>
2023-01-16 19:45:23 +1000

    Add files via upload
    Add task_1 and task_2

---
+++ task_2/hashtag_main.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+    string sharp = "#";
+    string str;
+    cout << "Vvedite stroku" << endl;
+    getline(cin,str);
+    try
+    {
+        int leng = str.length();
+        if ((leng <= 100) && (leng != 0))
+        {
+            Upper_Case(str);
+            cout << str << endl;
+            Delete_Spaces(str);
+            cout << str << endl;
+            str = sharp + str;
+            cout << str << endl;
+        }
+        else
+            throw(leng);
+    }
+    catch (int sizez)
+    {
+        cout << "ERROR: Yor string has more than 100 or 0 symbols." << endl;
+        cout << "Length of your string is " << sizez;
+    }
+}
+
