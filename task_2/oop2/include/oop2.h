ebc8364bb219ab570f0044529fac6f42969610c6
Andrew <724andkun@gmail.com>
2023-01-17 17:39:21 +1000

    Linking

---
+++ task_2/oop2/include/oop2.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <string>
+#include <algorithm>
+#include <ostream>
+using namespace std;
+
+
+void Upper_Case(string& str);
+
+void Delete_Spaces(string& str);
---
+++ task_2/oop2/src/oop2.cpp
@@ -0,0 +1,22 @@
+#include "oop2.h"
+#include <string>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+void Upper_Case(string& str)
+{
+    str[0] = (char)toupper(str[0]);
+    for (int i = 1; i < str.size(); i++)
+    {
+        if (str[i] == ' ')
+            str[i + 1] = (char)toupper(str[i + 1]);
+    }
+}
+
+
+void Delete_Spaces(string& str)
+{
+    string::iterator end_pos = remove(str.begin(), str.end(), ' ');
+    str.erase(end_pos, str.end());
+}
